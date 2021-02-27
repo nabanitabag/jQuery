e87b42e98a9c30e6146e74ee24b373dad4ddfda1
Nabanita Bag <ashley.makruz@gmail.com>
2021-02-27 21:48:24 +0530

    website animations using jquery

--- index.js
+++ index.js
@@ -1,4 +1,56 @@
 //jQuery()
 $(document).ready(function () {
-  $("h1").css("color", "red");
+  $("h1").addClass("margin-50 big-title");
+  console.log($("h1").css("color"));
+  // $("h1").removeClass("big-title");// margin-50");
+  $("button");
+
+
+$("h1").text("Greetings Planet");
+$("button").html("<em> I am benjamin button</em> ");
+
 });
+
+
+console.log($("img").attr("src"));
+$("a").attr("href", "https://yahoo.com");
+
+$("h1").click(function () {
+  $("h1").css("color", "purple");
+});
+
+// for(var  i = 0 ; i < 5 ; i++)
+// {
+//   document.querySelectorAll("button")[i].addEventListener("click", function () {
+//   document.querySelector("h1").style.color = "purple";
+// });
+// }
+$("button").click(function () {
+  $("h1").css("color", "purple");
+});
+
+$("input").keypress(function (event) {
+  $("h1").text(event.key);
+});
+
+//any dom event
+$("h1").on("mouseover", function() {
+  $("h1").css("color", "purple");
+});
+
+// $("button").remove();
+
+$("button").on("click", function () {
+  // $("h1").fadeToggle();
+  //.hide()
+  //.show()
+  //.fadeOut()
+  //.fadeIn()
+    //.slideToggle()
+
+  //only numbers
+  $("h1").slideUp().slideDown().animate({opacity: 0.5})
+  //.animate({margin: 10%});
+});
+
+//before, after, prepend, append
